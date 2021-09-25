b0b25f2cce09bee07ddcaae63e2c18d141d63c83
DhruvMitna19 <dhruvmitna9@gmail.com>
2021-09-25 18:44:47 +0530

    infrom random import choice, randintitial commit

---
+++ username.py
@@ -0,0 +1,33 @@
+from random import choice, randint
+
+username = ""
+
+with open("./english-adjectives.txt", "r") as a:
+
+    adjs = a.readlines()
+
+with open("./english-nouns.txt", "r") as b:
+
+    nouns = b.readlines()
+
+caps = input("Enter yes if you want the 2 words to be capitalized and no if you don't: ").lower()
+under = input("Enter yes if you want an underscore between the 2 words: ").lower()
+nums = input("Enter yes if you want numbers at the end of the username: ").lower()
+
+if caps != "no":
+
+    adj, noun = choice(adjs).capitalize(), choice(nouns).capitalize()
+
+else:
+
+    adj, noun = choice(adjs), choice(nouns)
+
+username += adj
+username += f"_{noun}" if under == "yes" else noun
+
+if nums != "no":
+
+    digits = int(input("Enter the number of digits that you want the number at the end of your username should be of: "))
+    username += "".join(str(randint(0,10)) for _ in range(digits))
+
+print(username)
